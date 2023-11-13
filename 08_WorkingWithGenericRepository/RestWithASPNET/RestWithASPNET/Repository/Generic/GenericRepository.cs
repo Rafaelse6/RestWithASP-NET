f919ddf015d1276f07530cc146db6d7dfbaf4deb
Rafael Santos <rafa.05125@gmail.com>
2023-11-13 14:04:37 -0300

    Implementing generic repository base structure

---
+++ 08_WorkingWithGenericRepository/RestWithASPNET/RestWithASPNET/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using RestWithASPNET.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public List<T> FindAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T FindById(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Create(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
