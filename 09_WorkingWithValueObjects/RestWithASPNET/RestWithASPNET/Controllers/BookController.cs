b2039c5c231b289533ed04b599f8c37b5907f9a4
Rafael Santos <rafa.05125@gmail.com>
2023-11-15 21:48:34 -0300

    Book business - VO to Entity conversion

--- 09_WorkingWithValueObjects/RestWithASPNET/RestWithASPNET/Controllers/BookController.cs
+++ 09_WorkingWithValueObjects/RestWithASPNET/RestWithASPNET/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RestWithASPNET.Business;
+using RestWithASPNET.Data.VO;
 using RestWithASPNET.Model;
 
 namespace RestWithASPNET.Controllers
@@ -36,14 +37,14 @@ namespace RestWithASPNET.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] Book book)
+        public IActionResult Post([FromBody] BookVO book)
         {
             if (book == null) return BadRequest();
             return Ok(_bookBusiness.Create(book));
         }
 
         [HttpPut]
-        public IActionResult Put([FromBody] Book book)
+        public IActionResult Put([FromBody] BookVO book)
         {
             if (book == null) return BadRequest();
             return Ok(_bookBusiness.Update(book));
