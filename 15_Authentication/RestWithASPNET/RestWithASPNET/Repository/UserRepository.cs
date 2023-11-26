4b6099edc9f7a0cb8662815e2dd3a14f6242b537
Rafael Santos <rafa.05125@gmail.com>
2023-11-25 21:11:25 -0300

    Token configuration

--- 15_Authentication/RestWithASPNET/RestWithASPNET/Repository/UserRepository.cs
+++ 15_Authentication/RestWithASPNET/RestWithASPNET/Repository/UserRepository.cs
@@ -2,6 +2,7 @@
 using RestWithASPNET.Model;
 using RestWithASPNET.Model.Context;
 using System;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -21,6 +22,29 @@ namespace RestWithASPNET.Repository
         {
             var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
             return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (!_context.Users.Any(u => u.Id.Equals(user.Id))) return null;
+            var result = _context.Users.SingleOrDefault(p => p.Id.Equals(user.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return result;
         }
 
         private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
