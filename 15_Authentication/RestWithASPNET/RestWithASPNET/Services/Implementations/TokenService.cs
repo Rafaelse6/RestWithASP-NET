6a06abb0248237fe234661e921a57a642f6be673
Rafael Santos <rafa.05125@gmail.com>
2023-11-26 21:35:27 -0300

    Implementing methods to generate RefreshToken

--- 15_Authentication/RestWithASPNET/RestWithASPNET/Services/Implementations/TokenService.cs
+++ 15_Authentication/RestWithASPNET/RestWithASPNET/Services/Implementations/TokenService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace RestWithASPNET.Services.Implementations
@@ -35,12 +36,36 @@ namespace RestWithASPNET.Services.Implementations
 
         public string GenerateRefreshToken()
         {
-            throw new System.NotImplementedException();
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new System.NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null
+                || !jwtSecurityToken.Header.Alg.Equals(
+                    SecurityAlgorithms.HmacSha256,
+                    StringComparison.InvariantCulture)) throw new SecurityTokenException("Invalid Token");
+
+            return principal;
         }
     }
 }
