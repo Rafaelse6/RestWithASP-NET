59911de059bafa9f0e9588c97fb12863aeec8bf3
Rafael Santos <rafa.05125@gmail.com>
2023-12-02 09:40:24 -0300

    Implementing Paged Search in GenericRepository

--- 17_QueryParamsAndPagedSearch/RestWithASPNET/RestWithASPNET/Repository/Generic/IRepository.cs
+++ 17_QueryParamsAndPagedSearch/RestWithASPNET/RestWithASPNET/Repository/Generic/IRepository.cs
@@ -16,5 +16,9 @@ namespace RestWithASPNET.Repository
         void Delete(long id);
 
         bool Exists(long id);
+
+        List<T> FindWithPagedSearch(string query);
+
+        int GetCount(string query);
     }
 }
